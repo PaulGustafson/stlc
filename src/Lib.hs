5617bfbf65b89fff780532c39205ee373fc673ed
Paul Gustafson <paul.gustafson@gmail.com>
2019-06-26 11:02:40 -0500

    Last commit before Ascription type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,11 +1,12 @@
 module Lib where
 
-data ExpA v t                      -- expression
+data ExpA v b                      -- expression of variable type v
+                                   --    and base type b
   = Var v                          -- variable
-  | Lit t                          -- literal
-  | Lam v (ExpA v t)               -- lambda
-  | App (ExpA v t) (ExpA v t)      -- apply
-  | Asc (ExpA v t) (TyA t)         -- ascribe
+  | Lit b                          -- literal
+  | Lam v (ExpA v b)               -- lambda
+  | App (ExpA v b) (ExpA v b)      -- apply
+  | Asc (ExpA v b) (TyA b)         -- ascription
   deriving (Show)
 
 type Exp = ExpA String Bool
@@ -15,16 +16,19 @@ type Exp = ExpA String Bool
 --     show (Lam s e) = "\\" + s + " -> " + (show e)
 --     show (App e1 e2) = "(" + (show e1) +") (" + (show e2) + ")"
 
-data TyA t                 -- Type
+data TyA b                 -- Types over a base type b
   = Atom 
-  | Fun (TyA t) (TyA t)    -- function
+  | Fun (TyA b) (TyA b)    -- function
   deriving (Show, Eq)
 
 type Ty = TyA Bool
 
-type Context v t = v -> Maybe t
+type Context v b = v -> Maybe (TyA b)
 
-append :: Eq v => (v, t) ->  Context v t -> Context v t
+type Failable t = Either t String
+
+
+append :: Eq v => (v, TyA b) ->  Context v b -> Context v b
 append h c = \ x -> case (c x) of
   Just a -> Just a
   Nothing -> if x == fst h
@@ -33,31 +37,39 @@ append h c = \ x -> case (c x) of
 
 
 -- Ascribes as many types as possible, given a context
-ascribe :: Context v t -> ExpA v t -> ExpA v t
+ascribe :: (Eq v, Show v, Show b) => Context v b -> ExpA v b -> Failable (ExpA v b)
 ascribe c e = case e of
   Var x -> case (c x) of
-    Just a -> Asc e a  --FIXME
+    Just a -> if check e a
+              then Left $ Asc e a
+              else Right $ "Type mismatch " ++ (show e) ++ "does not have type " ++ (show a)
     Nothing -> e
+  Lit l -> Left (Asc e Atom)
   Lam x e2 -> case (synth e) of
     Just f -> case f of
-      Fun a b -> Lam x (ascribe (append (x, a) c) e2)
-      _       -> error "Expected function"
-    _ -> e
+      Fun a b -> Left $ Lam x (ascribe (append (x, a) c) e2)
+      _       -> Right $ "Expected function"
+    Nothing -> Lam x (ascribe c e2)
   App e1 e2 -> case (synth e) of
-    Just a -> Asc (App (ascribe c e1) (ascribe c e2)) a
-    Nothing -> e
-  _ -> e
+    Just a -> Left $ Asc (App (ascribe c e1) (ascribe c e2)) a
+    Nothing -> Left $ App (ascribe c e1) (ascribe c e2)
+  Asc e2 t  -> if check e2 t
+               then case t of
+                      Atom -> e
+                      Fun a d -> Left $ Asc (ascribe (append
+  
+  
     
     
 
-synth :: ExpA v t -> Maybe (TyA t)
+synth :: ExpA v b ->  Maybe (TyA b)
 synth (Var x) = Nothing
 synth (Lit x) = Just Atom
 synth (Lam x e) = Nothing 
 synth (App x y) = case (synth x) of
   Just tx -> case tx of
-    Fun a b -> if (check y a)
-      then (Just b)
+    Fun a c -> if (check y a)
+      then (Just c)
       else Nothing
     _ -> Nothing
   _ -> Nothing
@@ -65,18 +77,18 @@ synth (Asc x y) = case (check x y) of
   True -> Just y
   False -> Nothing
 
-check :: ExpA v t -> TyA t -> Bool
+check :: ExpA v b -> TyA b -> Bool
 check (Var x) y = True
 check (Lit x) y = case y of 
      Atom -> True
      _    -> False
 check (Lam x e) y = case y of
-     Fun a b -> check e b 
+     Fun a c -> check e c 
      _       -> False
 check (App x y) z = case x of
      Lam w e -> case (synth x) of
        Just f -> case f of
-         Fun a b -> (check y a) && (b == z)
+         Fun a c -> (check y a) && (c == z)
          _       -> False
        _       -> False
      _       -> False
