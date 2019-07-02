f41fc660e81bc3df26f02e5b0e467cf80af953b5
Paul Gustafson <paul.gustafson@gmail.com>
2019-07-02 18:31:25 -0500

    Completed check function

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,18 +11,23 @@ data Ty t                          -- type on a base type enumeration $t$
 
 type Asc e t  = (e, Ty t)             -- ascription of expression to type
 
+
+type Error = String
+
+type Failable = Either Error
+
 type Checker e t = e -> t -> Bool
 
   
-data TExp v t e 
+data Exp v t e 
   -- typed expressions of variable type v, base type t, base expression type e
-  = TLit e t 
-  | TVar (Asc v t)
-  | TLam (Asc v t)  (Asc (TExp v t e) t)
-  | TApp (Asc (TExp v t e, TExp v t e) t)
+  = Lit e t 
+  | Var (Asc v t)
+  | Lam (Asc v t)  (Asc (Exp v t e) t)
+  | App (Asc (Exp v t e, Exp v t e) t)
 
 
-type Context v t = v -> Maybe (Ty t)
+type Context v t = v -> Failable (Maybe (Ty t))
 
 data PAtom t                        -- atomic partial types
   = Full (Ty t)
@@ -30,22 +35,23 @@ data PAtom t                        -- atomic partial types
 
 type PTy t = Ty (PAtom t)           -- partial types
 
-type PExp v t e = TExp v (PAtom t) e  -- partially typed expressions
-
-data IType = TBool 
-newtype IBool = IBool Bool           -- internal bool
-
-newtype Var = Var String
+type PExp v t e = Exp v (PAtom t) e  -- partially typed expressions
 
 type PContext v t = v -> PTy t
 
-type Error = String
-
-type Failable = Either Error
-
 type FComp v t = StateT (PContext v t) Failable   -- failable partial context
 
-type Exp = TExp String IType IBool 
+
+newtype SVar = SVar String
+
+data IType = TBool 
+
+newtype IBool = IBool Bool           -- internal bool
+
+type BExp = Exp SVar IType IBool     
+boolChecker :: Checker IBool IType
+boolChecker _ _ = True
+
 
 -- instance Show Exp where
 --     show (Var s) = s
@@ -72,35 +78,35 @@ type Exp = TExp String IType IBool
 --     Fun a b -> 
 
 
--- reifyE :: PExp v t e -> Maybe (TExp v t e)
--- reifyE (TLit pExp pAtom) =
+-- reifyE :: PExp v t e -> Maybe (Exp v t e)
+-- reifyE (Lit pExp pAtom) =
 --   case pAtom of
 --     Hole -> Nothing
 --     Full ty -> case ty of
---       Atom atomicType -> Just $ TLit pExp atomicType
+--       Atom atomicType -> Just $ Lit pExp atomicType
 --       Fun a b -> case pExp of
---         TLam x e2 -> Just $ TLam 
+--         Lam x e2 -> Just $ Lam 
   
   
 
--- reifyE :: PExp v -> Maybe (TExp v)
+-- reifyE :: PExp v -> Maybe (Exp v)
 -- reifyE (FullE e) = Just e
--- reifyE (PLit b) = Just $ TLit b
--- reifyE (PVar x pt) = liftM (TVar x) (reifyT pt)
--- reifyE (PLam x pt1 e pt2) = liftM3 (TLam x) (reifyT pt1) (reifyE e) (reifyT pt2)
--- reifyE (PApp e1 e2 pt) = liftM3 TApp (reifyE e1) (reifyE e2) (reifyT pt)
+-- reifyE (PLit b) = Just $ Lit b
+-- reifyE (PVar x pt) = liftM (Var x) (reifyT pt)
+-- reifyE (PLam x pt1 e pt2) = liftM3 (Lam x) (reifyT pt1) (reifyE e) (reifyT pt2)
+-- reifyE (PApp e1 e2 pt) = liftM3 App (reifyE e1) (reifyE e2) (reifyT pt)
 
 -- -- no FullE output
--- unreifyE :: TExp v -> PExp v
--- unreifyE (TVar x t) = PVar x (FullT t)
--- unreifyE TLit b = PLit b
--- unreifyE TLam x a e b = PLam (PVar 
+-- unreifyE :: Exp v -> PExp v
+-- unreifyE (Var x t) = PVar x (FullT t)
+-- unreifyE Lit b = PLit b
+-- unreifyE Lam x a e b = PLam (PVar 
 
 
 insert :: Eq v => (v, Ty t) -> Context v t -> Context v t
 insert h c = \ x ->
   if x == fst h
-  then Just (snd h)
+  then Right $ Just (snd h)
   else c x
 
 -- insertP :: Eq v => (v, PTy) -> PContext v -> PContext v
@@ -109,24 +115,6 @@ insert h c = \ x ->
 --   then snd h
 --   else c x
 
-
-typeof :: TExp v t e -> Ty t
-typeof (TLit exp ty) = Atom ty
-typeof (TVar (x, ty)) = ty
-typeof (TLam (x, t1) (exp, t2)) = Fun t1 t2
-typeof (TApp (_, ty)) = ty
-
-
-check :: (Eq t, Eq v) => Checker e t -> Context v t -> TExp v t e -> Bool
-check ch con texp = case texp of
-  TLit exp ty -> ch exp ty
-  TVar (x, ty) -> case con x of
-    Just t2 -> t2 == ty
-    Nothing -> True
-  TLam (x, t1) (exp, t2) -> check ch (insert (x, t1) con) exp
-  TApp ((e1, e2), ty) -> case e1 of
-    TLam (x, a) (e3, b) -> (a == typeof e2) && (b == ty) && (check ch con e1) && (check ch con e2)
-    _ -> False
 
 
 
@@ -150,8 +138,76 @@ check ch con texp = case texp of
 --     Atom -> throwError $ "Can't unify " ++ (show $ PFun a b) ++ " with atom"
 --     Fun a2 b2 ->  liftM2 (,) (unify a (FullT a2)) (unify b (FullT b2))
 
+unify :: (Eq t) => Ty t -> Ty t -> Failable (Ty t)
+unify (Atom t1) (Atom t2) =
+  if t1 == t2
+  then Right (Atom t1)
+  else throwError "Error in type unification"
+unify (Fun a1 b1) (Fun a2 b2) = liftM2 Fun (unify a1 b1) (unify a2 b2)
 
--- ascribe :: (Eq v, Show v) => PContext v -> PExp v -> (Failable (PExp v), PContext v)
+
+mergeM :: (Eq t) => Maybe (Ty t) -> Maybe (Ty t) -> Failable (Maybe (Ty t))
+mergeM mt1 mt2 = case (mt1, mt2) of
+  (Nothing, Nothing) -> Right Nothing
+  (Just t1, Nothing) -> Right $ Just t1
+  (Nothing, Just t2) -> Right $ Just t2
+  (Just t1, Just t2) ->
+    case unify t1 t2 of
+      Left err -> Left err
+      Right ty -> Right $ Just ty
+
+merge :: (Eq t) => Context v t -> Context v t -> Context v t
+merge c1 c2 = \ x -> join $ liftM2 mergeM (c1 x) (c2 x)
+      
+
+typeof :: Exp v t e -> Ty t
+typeof (Lit exp ty) = Atom ty
+typeof (Var (x, ty)) = ty
+typeof (Lam (x, t1) (exp, t2)) = Fun t1 t2
+typeof (App (_, ty)) = ty
+
+
+check :: (Eq t, Eq v) => Checker e t -> Context v t -> Exp v t e -> Failable (Context v t)
+check ch con texp = case texp of
+  Lit exp ty ->
+    if ch exp ty
+    then Right $ con
+    else throwError "Error typechecking literal"
+  Var (x, ty) -> case con x of
+    Left err -> Left err
+    Right mt -> case mt of
+      Just t2 ->
+        if t2 == ty
+        then Right $ con
+        else throwError "Error typechecking variable"
+      Nothing -> Right $ insert (x,ty) con
+  Lam (x, t1) (exp, t2) -> check ch (insert (x, t1) con) exp
+  App ((e1, e2), ty) -> case e1 of
+    Lam (x, a) (e3, b) ->
+      let
+        chE1 = check ch con e1
+        chE2 = check ch con e2
+      in
+        if (a == typeof e2) && (b == ty)
+        then liftM2 merge chE1 chE2
+        else throwError "Error in typechecking function application" 
+    _ -> throwError "Error typechecking application"
+
+
+
+
+
+-- ascribe :: (Eq v, Show v) => Checker e t -> PContext v t -> PExp v t e -> (Failable (PExp v t e), PContext v t)
+-- ascribe ch con exp = if check ch con exp
+
+
+
+  -- case exp of
+  -- Lit exp ty -> if ch exp ty
+  --               then (Right exp, con)
+  --               else (throwError "Type mismatch in ascription", ch)
+  -- Var (exp, ty) -> if check
+                      
 -- ascribe c fe@(FullE e) = ascribe c (unreifyE e)
 -- ascribe c (PVar x t) = case (unify (c x) t) of
 --   Left err -> (throwError err, insertP (x, Hole) c)
